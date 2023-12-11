0f760b96176192519191ef83edc4364b6e22696c
Rooders <lvxinglin163@163.com>
2023-12-11 18:19:55 +0800

    add some code from Mac

--- syn.sh
+++ syn.sh
@@ -3,6 +3,6 @@ git status
 git add *  
 git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
-git pull --rebase origin master   #domnload data
-git push origin master            #upload data
+git pull --rebase origin main   #domnload data
+git push origin main            #upload data
 git stash pop
